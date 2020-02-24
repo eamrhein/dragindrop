25e5cdc80f070bcb955db0e3a2579b08943023af
Eric Amrhein <eamrhein@protonmail.ch>
2020-02-24 15:08:48 -0800

    drag and drop tut

---
+++ src/data.js
@@ -0,0 +1,30 @@
+const data = {
+  tasks: {
+    one: {
+      id: "one",
+      content: "Hello"
+    },
+    two: {
+      id: "two",
+      content: "World"
+    },
+    three: {
+      id: "three",
+      content: "My Name"
+    },
+    four: {
+      id: "four",
+      content: "is Eric"
+    }
+  },
+  columns: {
+    one: {
+      id: "one",
+      title: "Not Started",
+      taskIds: ["one", "two", "three", "four"]
+    }
+  },
+  columnOrder: ["one"]
+};
+
+export default data;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,107 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import styled from "styled-components";
+import data from "./data";
+import "css-reset-and-normalize";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgray;
+  border-radius: 2px;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+`;
+const TaskContainer = styled.div`
+  border: 1px solid lightgray;
+  padding: 8px;
+  margin-bottom: 8px;
+  border-radius: 2px;
+  background-color: white;
+`;
+
+const Task = ({ task, index }) => {
+  return (
+    <Draggable draggableId={task.id.toString()} index={index}>
+      {provided => (
+        <TaskContainer
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          {task.content}
+        </TaskContainer>
+      )}
+    </Draggable>
+  );
+};
+
+const Column = ({ column, tasks }) => {
+  return (
+    <Container>
+      <Title>{column.title}</Title>
+      <Droppable droppableId={column.id.toString()}>
+        {provided => (
+          <TaskList {...provided.droppableProps} ref={provided.innerRef}>
+            {tasks.map((task, index) => (
+              <Task key={task.id} task={task} index={index} />
+            ))}
+            {provided.placeholder}
+          </TaskList>
+        )}
+      </Droppable>
+    </Container>
+  );
+};
+
+const App = () => {
+  let [state, setState] = useState(data);
+  const onDragEnd = result => {
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    const column = state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+    setState({
+      ...state,
+      columns: {
+        ...state.columns,
+        [newColumn.id]: newColumn
+      }
+    });
+  };
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      {state.columnOrder.map(columnId => {
+        const column = state.columns[columnId];
+        const tasks = column.taskIds.map(taskId => state.tasks[taskId]);
+        return <Column key={column.id} column={column} tasks={tasks} />;
+      })}
+    </DragDropContext>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
